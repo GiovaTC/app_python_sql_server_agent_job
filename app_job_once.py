8e3ed30378f703d8c0b7cace2f342a3614f9ffca
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-03 18:29:39 -0500

    segundo - commit

--- app_job_once.py
+++ app_job_once.py
@@ -1,6 +1,7 @@
 import pyodbc
 
-CONN_STR=(
+# Cadena de conexión a SQL Server
+CONN_STR = (
     "DRIVER={ODBC Driver 17 for SQL Server};"
     "SERVER=GTAPIERO-POLI;"
     "DATABASE=PruebaPython;"
@@ -11,29 +12,29 @@ CONN_STR=(
 
 def insertar_staging(nombre: str, edad: int, email: str):
     """
-    inserta en la tabla staging .
+    Inserta un registro en la tabla staging.
     """
     with pyodbc.connect(CONN_STR, autocommit=True) as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "INSERT INTO dbo.Usuarios_Staging (Nombre, Edad, Email) VALUES (?. ?, ?);",
+                "INSERT INTO dbo.Usuarios_Staging (Nombre, Edad, Email) VALUES (?, ?, ?);",
                 (nombre, edad, email)
             )
-        conn.commit()
     print(f"[STAGING] Insertado: {nombre}, {edad}, {email}")
 
 def ejecutar_job(job_name: str):
     """
-    ejecuta un job de sql server agent manualmente (una sola vez) .
+    Ejecuta un Job de SQL Server Agent manualmente (una sola vez).
     """
     with pyodbc.connect(CONN_STR, autocommit=True) as conn:
         with conn.cursor() as cur:
-            cur.execute("EXEC msdb.dbo.sp_start_job ? ", job_name)
-    print(f"[JOB] disparado: {job_name}")
+            cur.execute("EXEC msdb.dbo.sp_start_job ?", job_name)
+    print(f"[JOB] Disparado: {job_name}")
+
 if __name__ == "__main__":
-    # 1) inserta en staging .
+    # 1) Inserta en staging
     insertar_staging("Carlos Pérez", 28, "carlos.perez@exam.com.co")
     insertar_staging("Carlos A. Pérez", 29, "carlos.perez@exam.com.co")
-    # 2) ejecuta el job en una sola vez .
-    ejecutar_job("Job_Usuarios_Upsert_Once")    
-    
+
+    # 2) Ejecuta el Job una sola vez
+    ejecutar_job("Job_Usuarios_Upsert_Once")
